0248f6c9af202bb52baa9bab727714e8256b4318
Dominic Canare <dom@greenlightgo.org>
2020-12-30 17:32:28 -0600

    Animal assets

---
+++ VTLearn/Source/VTLearn/Public/VTAnimalConfig.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "VTAnimalConfig.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class VTLEARN_API UVTAnimalConfig : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UStaticMesh* BodyMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UStaticMesh* EarsMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FLinearColor BaseColor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UTexture2D* SkinTexture;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UTexture2D* FaceTexture;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UTexture2D* ArmTexture;
+
+};
